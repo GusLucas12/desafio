39b7fdb2744ba6bd682b10554df4e02c8d6d1572
Gustavo <gustavosilveira422.gl@gmail.com>
2024-05-27 19:22:07 -0300

    React(Pagina inicial home, concluida)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
-function Home(){
-    return(
-        <div>
-            <h1>Oi</h1>
+import styles from "./Home.module.css"
+import guy from "../images/guy.png"
+import { Link } from 'react-router-dom'
+function Home() {
+    return (
+        <div className={styles.central}>
+            <div className={styles.content}>
+                <Link><img src={guy} alt=""></img></Link>
+                <div className={styles.texto}>
+                    <h1>Cidadão <span className={styles.app} >APP</span></h1>
+                    <Link to='/'>
+                        <button>Iniciar</button>
+                    </Link>
+
+                </div>
+
+            </div>
         </div>
     );
 }
