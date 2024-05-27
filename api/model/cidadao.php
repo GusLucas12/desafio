48300c6fcca270d74afc63872e438c94a36a8a37
Gustavo <gustavosilveira422.gl@gmail.com>
2024-05-27 11:29:48 -0300

    API( Implementação MVC, criação da classe cidadao)

---
+++ api/model/cidadao.php
@@ -0,0 +1,46 @@
+<?php
+
+class Cidadao
+{
+    //definicoes 
+    private $conn;
+
+    public $id;
+    public $nome;
+    public $nis;
+
+    //construtor
+    public function __construct($banco)
+    {
+        $this->conn = $banco;
+    }
+
+    public function listar()
+    {
+        $query_cidadoes = "SELECT id,nome,nis FROM cidadao ORDER BY id DESC";
+        $result_cidadoes = $this->conn->prepare($query_cidadoes);
+        $result_cidadoes->execute();
+        return $result_cidadoes;
+    }
+
+    public function criar(){
+        $this->nis= $this->geradorNis();
+        
+        $query_cidadao = "INSERT INTO cidadao(nome,nis) VALUES (:nome,:nis)";
+        $cad_cidadao=$this->conn->prepare($query_cidadao);
+        
+
+        $cad_cidadao->bindParam(':nome',$this->nome,PDO::PARAM_STR);
+        $cad_cidadao->bindParam(':nis',$this->nis,PDO::PARAM_STR);
+    
+        $cad_cidadao->execute();
+    }
+
+    private function geradorNis(){
+        $nis='';
+        for($i=0;$i<11;$i++){
+            $nis .=mt_rand(0,9);
+        }
+        return $nis;
+    }
+}
