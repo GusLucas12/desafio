f9e0d7d31561a25d5bbcd5e01f1586d42cd4182f
Gustavo <gustavosilveira422.gl@gmail.com>
2024-05-27 10:18:08 -0300

    API(Cadastrar realizado e envio de resposta)

--- api/cadastrar.php
+++ api/cadastrar.php
@@ -6,3 +6,40 @@ header("Access-Control-Allow-Headers: *");
 
 //incluir conexão
 include_once 'conexao.php';
+
+//recebendo dados e decodificando o json
+$response_json =file_get_contents("php://input");
+$dados=json_decode($response_json,true);
+
+//se receber dados realiza a inserção
+if($dados){
+    $query_cidadao = "INSERT INTO cidadao(nome,nis) VALUES (:nome,:nis)";
+    $cad_cidadao=$conn->prepare($query_cidadao);
+
+    $cad_cidadao->bindParam(':nome',$dados['cidadao']['nome'],PDO::PARAM_STR);
+    $cad_cidadao->bindParam(':nis',$dados['cidadao']['nis'],PDO::PARAM_STR);
+
+    $cad_cidadao->execute();
+    //analisa se a inserção foi possivel
+    if($cad_cidadao->rowCount()){
+        $response=[
+            "erro"=>false,
+            "mensagem"=>"Cidadao Cadastrado com sucesso!"
+        ];
+    }else{
+        $response=[
+            "erro"=>true,
+            "mensagem"=>"Não foi possivel Cadastrar o Cidadao!"
+        ];
+    }
+}else{
+    $response=[
+        "erro"=>true,
+        "mensagem"=>"Não foi possivel Cadastrar o Cidadao!"
+    ];
+}
+
+//retorna a mensagem
+http_response_code(200);
+
+echo json_encode($response);
