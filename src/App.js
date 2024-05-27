c55f3f1e4fd8e035c97248a9d034f5b614f8d068
Gustavo <gustavosilveira422.gl@gmail.com>
2024-05-27 10:30:29 -0300

    React(Criação do Cadastrar e um input de Cadastro dentro do Banco de Dados)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import Listar from './pages/listar';
+import Cadastrar from './pages/cadastrar';
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Listar/>}/>
+        <Route path="/cadastrar" element={<Cadastrar/>}/>
       </Routes>
     </div>
   );
---
+++ src/pages/cadastrar.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+function Cadastrar(){
+    const [cidadao, setCidadao] = useState({
+        nome: '',
+        nis: ''
+      });
+      const [status,setStatus] = useState({
+        type:'',
+        mensagem:''
+      });
+      const valorInput = e => setCidadao({ ...cidadao, [e.target.name]: e.target.value });
+    
+      const cadCidadao = async e => {
+        e.preventDefault();
+        console.log(cidadao.nome);
+    
+        await fetch("http://localhost/desafio/cadastrar.php",{
+          method:'POST',
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body:JSON.stringify({cidadao})
+        })
+        .then((response)=> response.json())
+        .then((responseJson)=> {//console.log(responseJson)
+          if(responseJson.erro){
+            setStatus({
+              type:'erro',
+              mensagem: responseJson.mensagem
+            });
+          }else{
+            setStatus({
+              type:'sucess',
+              mensagem: responseJson.mensagem
+            });
+          }
+        } )
+        .catch(()=> {setStatus({type:'erro',mensagem:'Cidadao não Cadastrado com sucesso,tente mais tarde!'})})
+      }
+    
+    
+      return (
+        <div>
+          <h1>Cadastrar</h1>
+          {status.type === 'erro'? <p>{status.mensagem}</p>:" "}
+          {status.type === 'sucess'? <p>{status.mensagem}</p>:" "}
+          <form onSubmit={cadCidadao}>
+            <label>Nome: </label>
+            <input type="text" name="nome" placeholder="Nome do Cidadao" onChange={valorInput} /> <br />
+    
+            <label>NIS: </label>
+            <input type="text" name="nis" placeholder="NIS do cidadao" onChange={valorInput} /> <br />
+    
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+      );
+}
+export default Cadastrar;
