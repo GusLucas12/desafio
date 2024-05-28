c70a473c897c09a6aea3966d664a517eb1a31ef6
Gustavo <gustavosilveira422.gl@gmail.com>
2024-05-27 21:16:32 -0300

    React(criação header e css header)

---
+++ src/components/header.js
@@ -0,0 +1,33 @@
+import styles from './nav.module.css'
+import {Link} from 'react-router-dom'
+
+
+function Header(){
+    return(
+        <div>
+       <header>
+        <div className={styles.interface}>
+            <div className={styles.texto}>
+                <Link className={styles.link} to='/'>  <h1>Cidadão <span className={styles.app} >APP</span></h1></Link>
+          
+                
+                
+            </div> 
+
+            
+            <div className={styles.botao}>
+                <Link to="/cadastrar">
+                <button>Cadastro</button>
+                </Link>
+                
+            </div>
+
+        </div> 
+
+
+    </header>  
+      </div>
+    )
+}
+
+export default Header;
